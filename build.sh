5df2d6b768f69f813cc14abf9c30f3ca6d7ddfa5
VITAmin <mdrushd.alamin2020@vitstudent.ac.in>
2024-05-30 14:04:31 +0530

    zolo1 update

---
+++ build.sh
@@ -0,0 +1,12 @@
+# !/bin/bash
+
+if [ "$CF_PAGES_BRANCH" == "main" ]; then
+  # build production using base_url from the site config.toml
+  zola build
+elif [ "$CF_PAGES_BRANCH" == "staging" ]; then
+  # build staging using CF_STAGING_URL env
+  zola build --base-url $CF_STAGING_URL
+else
+  # build using the default cf pages env url
+  zola build --base-url $CF_PAGES_URL
+fi
